02f0b4d0d5bce9543abf25a48fdfcd43b8722cdc
beastsenior <26155865+beastsenior@users.noreply.github.com>
2021-11-26 23:47:48 +0800

    加入第2个球

--- test1.py
+++ test1.py
@@ -3,18 +3,26 @@ warnings.filterwarnings("ignore")
 
 import cmath
 import matplotlib.pyplot as plt
-import define as d
+import define
 
-a=1/cmath.sqrt(2)
-b=-1j/cmath.sqrt(2)
+#a=1/cmath.sqrt(2)
+#b=-1j/cmath.sqrt(2)
+
 #a=0
 #b=-1j
-#a=1
-#b=0
+
+#a=0
+#b=1
+
+a=1/cmath.sqrt(2)
+b=1/cmath.sqrt(2)
+c=1/cmath.sqrt(2)
+d=-1/cmath.sqrt(2)
 
 print("a, b:")
 print(a, b)
 
-d.plot_bloch_vector_by_ab(a, b)
+define.plot_bloch_vector_by_ab(a, b)
+define.plot_bloch_vector_by_ab(c, d)
 plt.show()
 plt.legend()
